0422fea17481baaa5a361f6a019d63e6d87dcefe
Tanish-Panwar <tanishpanwarrajput@gmail.com>
2026-04-24 12:51:40 +0530

    update

--- fastify.js
+++ fastify.js
@@ -1,15 +1,20 @@
-const app = require('fastify')({bodyLimit: 1024*1024});
+const app = require('fastify')({ bodyLimit: 1024 * 1024 });
 
-
-app.get('/api', (req, res) => {
-    res.json({messages: "hi"});
+app.get('/api', async (req, reply) => {
+    return { messages: "hi" };
 });
 
-app.listen({port:3000}, (err) => {
-    if(err) {
-        console.log(err.message);
+const start = async () => {
+    try {
+        await app.listen({
+            port: process.env.PORT || 3000,
+            host: '0.0.0.0'
+        });
+        console.log("Fastify server running");
+    } catch (err) {
+        console.error(err);
+        process.exit(1);
     }
-    else {
-        console.log("Fastify server running on port 3000");
-    }
-});
+};
+
+start();
